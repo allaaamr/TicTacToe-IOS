233f030befeb6daed215903c5ee4086eae89b061
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2022-08-23 11:31:31 +0200

    TicTacToe IOS Game

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -8,9 +8,92 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var moves = ["","","","","","","","",""]
+    @State private var count = 0
+    @State private var endGameText = "TicTacOrange"
+    @State private var gameEnded = false
+    private var ranges = [(0..<3),(3..<6),(6..<9)]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            Color.black
+                .ignoresSafeArea()
+         VStack {
+             Text(endGameText)
+                 .foregroundColor(.orange)
+                 .font(.system(size: 30))
+                 .alert(endGameText, isPresented: $gameEnded) {
+                     Button("Reset", role: .destructive, action: resetGame)
+                 }
+             Spacer()
+             ForEach(ranges, id: \.self) { range in
+                 HStack {
+                     ForEach(range, id: \.self) { i in
+                         XOButton(letter: $moves[i])
+                             .simultaneousGesture(
+                                 TapGesture()
+                                     .onEnded { _ in
+                                         print("Tap: \(i)")
+                                         playerTap(index: i)
+                                     }
+                             )
+                     }
+                 }
+             }
+             Spacer()
+             Button("Reset") {
+                 resetGame()
+             }
+         }
+        }
+     }
+    func resetGame() {
+            endGameText = "TicTacOrange"
+            moves = ["","","","","","","","",""]
+    }
+        
+    func playerTap(index: Int) {
+        count += 1
+        if moves[index] == ""{
+            moves[index] = "X"
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) { // Change `2.0` to the desired number of seconds.
+                botMove()
+            }
+        }
+
+        if checkWinner(list: moves, letter: "X") {
+                endGameText = "YOU WON!"
+                print(endGameText)
+                gameEnded = true
+        }
+        
+        
+        
+    }
+    func botMove(){
+        count += 1
+        var availableMoves: [Int] = []
+        var movesLeft = 0
+                
+        // Check the available moves left
+        for move in moves {
+            if move == "" {
+                availableMoves.append(movesLeft)
+            }
+            movesLeft += 1
+        }
+        
+        // Make sure there are moves left before bot moves
+        if availableMoves.count != 0 {
+            moves[availableMoves.randomElement()!] = "O"
+        }
+        if checkWinner(list: moves, letter: "O") {
+            endGameText = "You Lost! Try Again Tomorrow"
+        }
+        else if (count == 9){
+            endGameText = "It's a tie!"
+            print("Tie")
+        }
     }
 }
 
