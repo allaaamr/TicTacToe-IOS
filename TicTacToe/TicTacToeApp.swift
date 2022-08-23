a85a391d4134dc3bc2dc05e05ffb982934c5c1fc
Alaa Amr Mansour <alaa.amr1@hotmail.com>
2022-08-23 10:33:50 +0200

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Alaa Amr Abdelazeem on 23/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
